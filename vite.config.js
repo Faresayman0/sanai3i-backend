81e67a9309d960c4b81f796b3861ace6e96ab76d
Faresayman0 <faresayman33233@gmail.com>
2024-05-25 12:05:15 +0300

    Updated project files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        port: process.env.PORT || 3000,
+        host: true, // يجعل التطبيق متاحًا على الشبكة
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
